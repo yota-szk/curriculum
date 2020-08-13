c9f0df2a193f3468ba424724f3bb055b98ee11c1
yota-szk <yota.s.create@gmail.com>
2020-08-13 22:23:42 +0900

    mainメソッド・インスタンス・メソッドの呼び出し
    よろしくお願い致します

--- JavaStudy/2-2/src/study/Study.java
+++ JavaStudy/2-2/src/study/Study.java
@@ -1,10 +1,32 @@
 package study;
 
-public class Study {
+/**
+ * Study.java
+ */
+class Account {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    // フィールド変数
+    private String userName;
+    private int id;
+    private String password;
 
-	}
+    // コンストラクタ
+    public Account(String userName, int id, String password) {
+        this.userName = userName;
+        this.id = id;
+        this.password = password;
+    }
+
+    // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
+    // アクセス修飾子:「protected」
+    // メソッド名:「printAccountInfo」
+    protected void printAccountInfo () {
+    	System.out.println("ユーザー名は、" + userName);
+    	System.out.println("IDは、" + id);
+    	System.out.println("パスワードは、" + password);
+    }
+
+
+
 
 }
